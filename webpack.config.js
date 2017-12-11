fb0b1ab9bfa5f96b9ef9d1f0eab70f9f74b3cbc5
Alexey Tuboltsev <at@wohlgemuth-team.de>
2017-12-11 15:48:55 +0100

    rebuilt and repackaged to save weight

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,61 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const path = require('path')
+const ClosureCompilerPlugin = require('webpack-closure-compiler');
+const webpack = require('webpack')
 
 const config = {
-  context: path.resolve(__dirname, './src'),
-  devtool: 'cheap-module-source-map',
-  entry: ['babel-polyfill', './index.js'],
-  output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, './dist'),
-    library: 'aws-cognito-redux-saga',
-    libraryTarget: 'umd'
-  },
-  externals: [
-    'react',
-    'redux-saga/effects',
-    'amazon-cognito-identity-js',
-    'aws-sdk'
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['es2015', 'stage-0'],
-          plugins: ['transform-react-jsx']
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader']
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ['file-loader']
-      },
-      {
-        test: /\.(csv|tsv)$/,
-        use: ['csv-loader']
-      },
-      {
-        test: /\.xml$/,
-        use: ['xml-loader']
-      }
+    context: path.resolve(__dirname, './src'),
+    devtool: 'cheap-module-source-map',
+    entry: ['./index.js'],
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, './dist'),
+        library: 'aws-cognito-redux-saga',
+        libraryTarget: 'umd'
+    },
+    externals: [
+        'react',
+        'redux-saga/effects',
+        'amazon-cognito-identity-js',
+        'aws-sdk',
+        'aws-cognito-promises',
+        'babel-polyfill'
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['stage-0'],
+                    plugins: ['transform-react-jsx']
+                }
+            },
+            {
+                test: /\.(csv|tsv)$/,
+                use: ['csv-loader']
+            },
+            {
+                test: /\.xml$/,
+                use: ['xml-loader']
+            }
+        ]
+    },
+    plugins: [
+        new BundleAnalyzerPlugin(),
+        new webpack.DefinePlugin({
+            "process.env": { NODE_ENV: "'production'" }
+        }),
+        new ClosureCompilerPlugin({
+            compiler: {
+                language_in: 'ECMASCRIPT6',
+                language_out: 'ECMASCRIPT5',
+                compilation_level: 'SIMPLE'
+            },
+            concurrency: 3,
+        })
     ]
-  }
 }
 
 module.exports = config
